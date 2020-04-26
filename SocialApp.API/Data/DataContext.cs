bc1e34563cf6477f083d183706fd90e87938f818
Moiyd AlMokayad <moiyd_hamed@hotmail.com>
2020-04-26 21:56:22 +0300

    Initial Commit

---
+++ SocialApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+
+using Microsoft.EntityFrameworkCore;
+using SocialApp.API.Models;
+
+namespace SocialApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+        public DbSet<Value> Values {get; set;} 
+    // this (Values) represent the table name gonna be in the Database
+    // and Value represent the class defined in the Models folder
+    }
+} 
