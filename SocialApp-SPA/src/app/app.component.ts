4164e20358f8d9b6111475301269cb0274dcba44
Moiyd AlMokayad <moiyd_hamed@hotmail.com>
2020-05-15 16:00:31 +0300

    Some sweet changes on Auth

--- SocialApp-SPA/src/app/app.component.ts
+++ SocialApp-SPA/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService } from './_services/alertify.service';
+import { AuthService } from './_services/auth.service';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import { TestObject } from 'protractor/built/driverProviders';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Moiyds App';
+export class AppComponent implements OnInit {
+  // = 'Moiyds App';
+  jwtHelper = new JwtHelperService();
+
+
+  constructor(private authService: AuthService){}
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authService.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 }
