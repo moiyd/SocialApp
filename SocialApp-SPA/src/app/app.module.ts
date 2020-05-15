b111510b6c9a7dd97b8288b87a7b40e35d5d6490
Moiyd AlMokayad <moiyd_hamed@hotmail.com>
2020-05-15 17:39:45 +0300

    End of Sec 6

--- SocialApp-SPA/src/app/app.module.ts
+++ SocialApp-SPA/src/app/app.module.ts
@@ -2,14 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+
 
 import { AppComponent } from './app.component';
-import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptor, ErrorInterceptorProvidor } from './_services/error.interceptor';
+
 
 @NgModule({
    declarations: [
@@ -21,7 +26,9 @@ import { ErrorInterceptor, ErrorInterceptorProvidor } from './_services/error.in
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BrowserAnimationsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
             ErrorInterceptorProvidor
